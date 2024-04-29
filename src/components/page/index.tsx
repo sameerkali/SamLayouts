9d497aa1323611f381fe93330e97f993b172f0fe
Sameer Faridi <s.faridi007@gmail.com>
2024-04-29 20:59:15 +0530

    add testing in this library

--- src/components/page/index.tsx
+++ src/components/page/index.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import {PageProps} from './types'
-const Page: React.FC<PageProps> = ({title, children}) => {
-  return (
-    <>
-    <h1>{title}</h1>
-    {children}
-    </>
-  )
-}
+import React from "react";
+import { type PageProps } from "./types";
+import { Container } from "./styled";
 
-export default Page
+const Page: React.FC<PageProps> = ({ title, children }) => {
+  return (
+    <Container data-testid="page-container">
+      <h1>{title}</h1>
+      <>{children}</>
+    </Container>
+  );
+};
+
+export default Page;
