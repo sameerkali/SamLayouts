fb2dfacf34dfe4a4f859643ec5ed038e90020fe8
Sameer Faridi <s.faridi007@gmail.com>
2024-04-29 22:32:08 +0530

    make a build using huskey

---
+++ dist/cjs/types/components/page/styled.d.ts
@@ -0,0 +1,2 @@
+/// <reference types="react" />
+export declare const Container: import("styled-components").IStyledComponent<"web", import("styled-components/dist/types").FastOmit<import("react").DetailedHTMLProps<import("react").HTMLAttributes<HTMLDivElement>, HTMLDivElement>, never>>;
--- dist/esm/types/components/page/index.d.ts
+++ dist/esm/types/components/page/index.d.ts
@@ -1,4 +1,4 @@
-import React from 'react';
-import { PageProps } from './types';
+import React from "react";
+import { type PageProps } from "./types";
 declare const Page: React.FC<PageProps>;
 export default Page;
